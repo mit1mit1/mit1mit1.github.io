75082a8638484dbf4066ea3a9a6338f1f870af82
mit1mit1 <mit1mit1@users.noreply.github.com>
2024-03-17 05:34:46 +0000

    deploy: b5d35497ec7665f329fd87e570281572274fcf2f

--- _app/immutable/chunks/2.05a6d433.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.80f692d6.js";export{t as component};
---
+++ _app/immutable/chunks/paths.6d34e036.js
@@ -0,0 +1,1 @@
+var s;const t=((s=globalThis.__sveltekit_tlkllo)==null?void 0:s.base)??"";var l;const a=((l=globalThis.__sveltekit_tlkllo)==null?void 0:l.assets)??t;export{a,t as b};
