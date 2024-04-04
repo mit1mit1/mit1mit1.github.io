d103eafd1db97e928c272d1a573bf8cd70b203fc
Mitchell (Midly) Roberts <mitchell.roberts11@gmail.com>
2024-04-05 08:04:55 +1100

    chore: add routes

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import Projects from '../../components/Projects.svelte';
+</script>
+
+<Projects />
---
+++ src/routes/support/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import SupportUs from '../../components/SupportUs.svelte';
+</script>
+
+<SupportUs />
